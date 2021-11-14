49aa6a6b4149dfb29f8f445d23cb2b4e613e7bc3
Kirill S <kirill-samsonov@bk.ru>
2021-11-14 22:33:14 +0300

    checkbox component added

---
+++ TrashGrounds/Components/Checkbox.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrashGrounds.Components
+{
+    public class Checkbox : ViewComponent
+    {
+        public IViewComponentResult Invoke(string label, string type = "checkbox")
+        {
+            return View(type, label);
+        }
+    }
+}
