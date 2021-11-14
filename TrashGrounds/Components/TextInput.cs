fa023030663239cef2cbb7b996c701599b772047
Kirill S <kirill-samsonov@bk.ru>
2021-11-14 23:28:25 +0300

    text input component refactored

--- TrashGrounds/Components/TextInput.cs
+++ TrashGrounds/Components/TextInput.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TrashGrounds.Models;
 
 namespace TrashGrounds.Components
 {
     public class TextInput : ViewComponent
     {
-        public IViewComponentResult Invoke(string type, string label)
+        public IViewComponentResult Invoke(string type = "text", string label = "")
         {
-            return View(type, label);
+            return View("~/Views/CarbonComponents/TextInput.cshtml", new TextInputViewModel(type, label));
         }
     }
 }
---
+++ TrashGrounds/Models/TextInputViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TrashGrounds.Models
+{
+    public class TextInputViewModel
+    {
+        public TextInputViewModel(string type, string label)
+        {
+            Type = type;
+            Label = label;
+        }
+
+        public string Type { get; set; } = "primary";
+
+        public string Label { get; set; } = "";
+    }
+}
