1aee0ceb2dc312ed3ec9bb7b18d2de37178bd8b0
Kirill S <kirill-samsonov@bk.ru>
2021-11-14 23:06:36 +0300

    button component refactored

--- TrashGrounds/Components/Button.cs
+++ TrashGrounds/Components/Button.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TrashGrounds.Models;
 
 namespace TrashGrounds.Components
 {
     public class Button : ViewComponent
     {
-        public IViewComponentResult Invoke(string type, string text)
+        public IViewComponentResult Invoke(string type = "primary", string text = "", string size = "large")
         {
-            return View(type, text);
+            return View("~/Views/CarbonComponents/Button.cshtml", new ButtonViewModel(type, text, size));
         }
     }
 }
---
+++ TrashGrounds/Models/ButtonViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TrashGrounds.Models
+{
+    public class ButtonViewModel
+    {
+        public ButtonViewModel(string type, string text, string size)
+        {
+            Type = type;
+            Text = text;
+            Size = size;
+        }
+
+        public string Type { get; set; } = "primary";
+
+        public string Text { get; set; } = "";
+
+        public string Size { get; set; } = "large";
+    }
+}
