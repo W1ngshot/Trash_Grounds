7a872f3cd493f9d59aecdf0204f648fbb19ceec0
Kirill S <kirill-samsonov@bk.ru>
2021-11-15 00:27:54 +0300

    text area component added

---
+++ TrashGrounds/Components/TextArea.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrashGrounds.Components
+{
+    public class TextArea : ViewComponent
+    {
+        public IViewComponentResult Invoke(string label)
+        {
+            return View("~/Views/CarbonComponents/TextArea.cshtml", label);
+        }
+    }
+}
