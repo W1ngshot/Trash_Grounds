e3a44dd7fb3bbcec4c3d3e3c2888cbb6213c69cb
Kirill S <kirill-samsonov@bk.ru>
2021-11-14 22:31:55 +0300

    button component added

---
+++ TrashGrounds/Components/Button.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrashGrounds.Components
+{
+    public class Button : ViewComponent
+    {
+        public IViewComponentResult Invoke(string type, string text)
+        {
+            return View(type, text);
+        }
+    }
+}
