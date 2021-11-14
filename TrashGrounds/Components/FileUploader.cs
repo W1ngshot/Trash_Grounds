df6fcf5da5cd22a578e24606f3fc3a5422220ff6
Kirill S <kirill-samsonov@bk.ru>
2021-11-15 00:44:47 +0300

    file uploader component added

---
+++ TrashGrounds/Components/FileUploader.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrashGrounds.Models;
+
+namespace TrashGrounds.Components
+{
+    public class FileUploader : ViewComponent
+    {
+        public IViewComponentResult Invoke(string description = "", string label = "")
+        {
+            return View("~/Views/CarbonComponents/FileUploader.cshtml", new FileUploaderViewModel(label, description));
+        }
+    }
+}
---
+++ TrashGrounds/Models/FileUploaderViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TrashGrounds.Models
+{
+    public class FileUploaderViewModel
+    {
+        public FileUploaderViewModel(string label, string description)
+        {
+            Label = label;
+            Description = description;
+        }
+
+        public string Label { get; set; } = "";
+        
+        public string Description { get; set; } = "";
+    }
+}
