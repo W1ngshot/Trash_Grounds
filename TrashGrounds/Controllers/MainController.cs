878d2d3c14575edc410ad34924d8feed0785aa54
Kirill S <kirill-samsonov@bk.ru>
2021-11-15 10:58:32 +0300

    refactored some files

--- TrashGrounds/Controllers/MainController.cs
+++ TrashGrounds/Controllers/MainController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace TrashGrounds.Controllers
 {
     public class MainController : Controller
     {
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
         public IActionResult Index()
         {
             return View();
