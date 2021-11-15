c65e5473a442ee85a4aa589d66aff3c1ed409849
Kirill S <kirill-samsonov@bk.ru>
2021-11-15 11:26:39 +0300

    track component added

---
+++ TrashGrounds/Components/TrackCard.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrashGrounds.Models;
+using System;
+
+namespace TrashGrounds.Components
+{
+
+    public class TrackCard : ViewComponent
+    {
+        public IViewComponentResult Invoke(
+            double? rating = null,
+            string? previewUrl = null,
+            long trackId = 0,
+            string trackName = "Track name",
+            long authorId = 0,
+            string authorName = "Author name")
+        {
+            var model = new TrackCardViewModel
+            {
+                TrackId = trackId,
+                TrackName = trackName,
+                AuthorId = authorId,
+                AuthorName = authorName,
+                PreviewUrl = previewUrl
+            };
+
+            if (rating != null)
+                model.Rating = String.Format("{0:0.0}", rating);
+
+
+            return View(model);
+        }
+    }
+}
---
+++ TrashGrounds/Models/TrackCardViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TrashGrounds.Models
+{
+
+    public class TrackCardViewModel
+    {
+        public long TrackId { get; set; } = 0;
+        public string TrackName { get; set; } = nameof(TrackName);
+        public long AuthorId { get; set; } = 0;
+        public string AuthorName { get; set; } = nameof(AuthorName);
+        public string? PreviewUrl { get; set; }
+        public string? Rating { get; set; }
+    }
+}
