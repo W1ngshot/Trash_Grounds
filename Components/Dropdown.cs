fbdfeee6c15a7a70cfb5a21df3dd4ae2ebdedd61
Kirill S <kirill-samsonov@bk.ru>
2021-11-16 23:23:28 +0300

    dropdown component was added

--- Components/Dropdown.cs
+++ Components/Dropdown.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TrashGrounds.Models;
 
 namespace TrashGrounds.Components
 {
     public class Dropdown : ViewComponent
     {
-        public IViewComponentResult Invoke(string label = "")
+        public IViewComponentResult Invoke(IEnumerable<DropdownOption> options, string label = "")
         {
-            return View("~/Views/CarbonComponents/Dropdown.cshtml");
+            return View("~/Views/CarbonComponents/Dropdown.cshtml", new DropdownViewModel(label, options));
         }
     }
 }
