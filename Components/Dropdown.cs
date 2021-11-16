d439dbdd23ca40c673ebe9cc3da1810c2e68308e
Kirill S <kirill-samsonov@bk.ru>
2021-11-16 21:46:06 +0300

    moved project out of TrashGrounds folder

---
+++ Components/Dropdown.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrashGrounds.Components
+{
+    public class Dropdown : ViewComponent
+    {
+        public IViewComponentResult Invoke(string label = "")
+        {
+            return View("~/Views/CarbonComponents/Dropdown.cshtml");
+        }
+    }
+}
