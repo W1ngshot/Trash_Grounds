2f7798e4e73b1166bf71f0b918047fc2bc6b9149
Kirill S <kirill-samsonov@bk.ru>
2021-11-20 23:57:04 +0300

    DB added

---
+++ DataBase/DBClasses/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TrashGrounds.DataBase.DBClasses
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+        
+        public string Password { get; set; }
+        
+        public string Nickname { get; set; }
+        
+        public int Avatar_asset_id { get; set; }
+        
+        public DateTime Reg_date { get; set; }
+    }
+}
